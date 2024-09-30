545da6d395d744b70c3cb85d51387b2ccfef2e59
hi-san <h.ahkh10@icloud.com>
2024-09-30 19:42:23 +0900

    店舗詳細ページセレクト追加

--- src/resources/views/detail.blade.php
+++ src/resources/views/detail.blade.php
@@ -39,6 +39,17 @@
                 <div class="form__inner time">
                     <select name="time" id="rese_time" onchange="timeChange()">
                         <option value="">時間</option>
+                        <option value="00:00">00:00</option>
+                        <option value="01:00">01:00</option>
+                        <option value="02:00">02:00</option>
+                        <option value="03:00">03:00</option>
+                        <option value="04:00">04:00</option>
+                        <option value="05:00">05:00</option>
+                        <option value="06:00">06:00</option>
+                        <option value="07:00">07:00</option>
+                        <option value="08:00">08:00</option>
+                        <option value="09:00">09:00</option>
+                        <option value="10:00">10:00</option>
                         <option value="11:00">11:00</option>
                         <option value="12:00">12:00</option>
                         <option value="13:00">13:00</option>
@@ -48,6 +59,10 @@
                         <option value="17:00">17:00</option>
                         <option value="18:00">18:00</option>
                         <option value="19:00">19:00</option>
+                        <option value="20:00">20:00</option>
+                        <option value="21:00">21:00</option>
+                        <option value="22:00">22:00</option>
+                        <option value="23:00">23:00</option>
                     </select>
                 </div>
                 <div class="error__message">
@@ -64,6 +79,10 @@
                         <option value="4">4人</option>
                         <option value="5">5人</option>
                         <option value="6">6人</option>
+                        <option value="7">7人</option>
+                        <option value="8">8人</option>
+                        <option value="9">9人</option>
+                        <option value="10">10人</option>
                     </select>
                 </div>
                 <div class="error__message">
