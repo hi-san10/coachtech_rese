cb9ee04f5fd10abca96f2fb9766a67a2d275ef48
hi-san <h.ahkh10@icloud.com>
2024-11-07 17:38:23 +0900

    userfactory作成

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,9 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PrefecturesTableSeeder::class);
-        $this->call(GenresTableSeeder::class);
-        $this->call(RestaurantsTableSeeder::class);
+        // $this->call(PrefecturesTableSeeder::class);
+        // $this->call(GenresTableSeeder::class);
+        // $this->call(RestaurantsTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
+        User::factory(20)->create();
     }
 }
