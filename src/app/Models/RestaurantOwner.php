0d5ce0d43c040c5d13004e3f627df4491c7713e1
hi-san <h.ahkh10@icloud.com>
2024-11-14 21:21:42 +0900

    コントローラ名修正

--- src/app/Models/RestaurantOwner.php
+++ src/app/Models/RestaurantOwner.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Restaurant_owner extends Model
+class RestaurantOwner extends Model
 {
     use HasFactory;
 
