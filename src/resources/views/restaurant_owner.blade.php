ec8d1ec3c94d832d8d130fd496aaabd83d0dac7e
hi-san <h.ahkh10@icloud.com>
2024-11-14 21:10:12 +0900

    コントローラ名修正

--- src/resources/views/restaurant_owner.blade.php
+++ src/resources/views/restaurant_owner.blade.php
@@ -7,10 +7,16 @@
 @section('content')
 <div class="rese-restaurant_owner">
     <div class="restaurant_owner__content">
-        <div class="create_restaurant"><img src="{{ asset('storage/sushi.jpg') }}" alt=""></div>
-        <div class="update_restaurant"></div>
-        <div class="reservation_confirm"></div>
-        <div class="owner_setting"></div>
+        <div class="create_restaurant">
+            <a href="">店舗作成</a>
+        </div>
+        <div class="update_restaurant">
+            <a href="">店舗情報編集</a>
+        </div>
+        <div class="reservation_confirm">
+            <a href="">予約情報確認</a>
+        </div>
+        <div class="owner_setting">パスワード変更</div>
     </div>
 </div>
 @endsection
