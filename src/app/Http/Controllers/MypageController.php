d4808847bed110666924ce25b973206b1c82537a
hi-san <h.ahkh10@icloud.com>
2024-11-17 10:32:46 +0900

    quコードurl修正

--- src/app/Http/Controllers/MypageController.php
+++ src/app/Http/Controllers/MypageController.php
@@ -57,9 +57,9 @@ class MypageController extends Controller
     public function qr_code(Request $request)
     {
         $url = config('app.url').'/'.'my_reservation/'.$request->reservation_id;
-        // $qr_code = QrCode::size(200)->generate('ec2-54-65-253-1.ap-northeast-1.compute.amazonaws.com'.'/my_reservation'.'/'.$request->reservation_id);
-        $qr_code = QrCode::size(200)->generate($url);
+        $qr_code = QrCode::size(200)->generate('ec2-54-65-253-1.ap-northeast-1.compute.amazonaws.com'.'/my_reservation'.'/'.$request->reservation_id);
+        $data = '/my_reservaion'.'/'.$request->reservation_id;
         // dd($qr_code);
-        return view('qr_code', compact('qr_code'));
+        return view('qr_code', compact('qr_code', 'data'));
     }
 }
