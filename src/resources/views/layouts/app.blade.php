306b3499cabb6eee82126285a0d38095692e8686
hi-san <h.ahkh10@icloud.com>
2025-04-04 18:20:19 +0900

    reset.css変更

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rese</title>
-    <link rel="stylesheet" href="https://unpkg.com/ress@4.0.0/dist/ress.min.css">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     <script src="https://kit.fontawesome.com/9c882e03ff.js" crossorigin="anonymous"></script>
     @yield('css')
