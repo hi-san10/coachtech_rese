045d5dd3673cd8dcb1105c01ce2ef0573016e863
hi-san <h.ahkh10@icloud.com>
2025-04-12 15:01:11 +0900

    レビューするページのレスポンシブ対応

--- src/resources/views/review.blade.php
+++ src/resources/views/review.blade.php
@@ -13,16 +13,26 @@
                 <h2 class="shop_name">{{ $shop_name }}の評価</h2>
                 <p>[評価段階]</p>
                 <div class="review_point">
-                    <input type="radio" value="1" name="review_point" id="1" value="1">
-                    <label for="1"> 1:悪い</label>
-                    <input type="radio" value="2" name="review_point" id="2" value="2">
-                    <label for="2"> 2:少し悪い</label>
-                    <input type="radio" value="3" name="review_point" id="3" value="3">
-                    <label for="3"> 3:普通</label>
-                    <input type="radio" value="4" name="review_point" id="4" value="4">
-                    <label for="4"> 4:まあまあ良い</label>
-                    <input type="radio" value="5" name="review_point" id="5" value="5">
-                    <label for="5"> 5:良い</label>
+                    <div class="review_item">
+                        <input type="radio" value="1" name="review_point" id="1" value="1">
+                        <label for="1"> 1:悪い</label>
+                    </div>
+                    <div class="review_item">
+                        <input type="radio" value="2" name="review_point" id="2" value="2">
+                        <label for="2"> 2:少し悪い</label>
+                    </div>
+                    <div class="review_item">
+                        <input type="radio" value="3" name="review_point" id="3" value="3">
+                        <label for="3"> 3:普通</label>
+                    </div>
+                    <div class="review_item">
+                        <input type="radio" value="4" name="review_point" id="4" value="4">
+                        <label for="4"> 4:まあまあ良い</label>
+                    </div>
+                    <div class="review_item">
+                        <input type="radio" value="5" name="review_point" id="5" value="5">
+                        <label for="5"> 5:良い</label>
+                    </div>
                 </div>
             </div>
             <div class="review-content__comment">
