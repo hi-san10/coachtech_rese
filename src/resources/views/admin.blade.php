ede75956dea523fcb6cdad4ed26b27fa3d2f8675
hi-san <h.ahkh10@icloud.com>
2025-04-12 17:56:42 +0900

    店舗代表者作成画面のレスポンシブ対応

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -10,22 +10,22 @@
         <div class="rese-admin__content-inner">
             <form class="content-inner__form" action="/admin/store" method="post">
                 @csrf
-                <input type="text" name="name" placeholder="Name">
+                <input class="form__item" type="text" name="name" placeholder="Name">
                 @error('name')
                 <span class="error_message">{{ $message }}</span>
                 @enderror
-                <input type="email" name="email" placeholder="Email">
+                <input class="form__item" type="email" name="email" placeholder="Email">
                 @error('email')
                 <span class="error_message">{{ $message }}</span>
                 @enderror
-                <input type="password" name="password" placeholder="Password">
+                <input class="form__item" type="password" name="password" placeholder="Password">
                 @error('password')
                 <span class="error_message">{{ $message }}</span>
                 @enderror
                 <button class="form__btn">店舗代表者作成</button>
             </form>
         </div>
-        <div class="rese-admin__content-inner">
+        <div class="rese-admin__content-inner link">
             <a href="/admin/notice/mail">お知らせメールを送信する</a>
         </div>
     </div>
